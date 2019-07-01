caeb74659e3e93dfb7afece5624cd5c69fb6b184
Patrick Elsen <pelsen@xfbs.net>
2019-07-01 21:35:19 +0200

    implements error messaging

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use ipquery::*;
 
 fn main() {
     let provider = Provider::default();
-    println!("{:?}", provider.query_ipv4());
-    println!("{:?}", provider.query_ipv6());
+    match provider.query_ipv4() {
+        Ok(ip) => println!("IPv4 = {}", ip),
+        Err(e) => println!("IPv4 lookup error: {}", e),
+    }
+
+    match provider.query_ipv6() {
+        Ok(ip) => println!("IPv6 = {}", ip),
+        Err(e) => println!("IPv6 lookup error: {}", e),
+    }
 }
