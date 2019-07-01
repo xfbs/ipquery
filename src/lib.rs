a66521e57a7cc32101014f5c999e7cfccfd5436e
Patrick Elsen <pelsen@xfbs.net>
2019-07-01 21:06:28 +0200

    removes error-chain

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
-use error_chain::{bail, error_chain};
 use std::net::*;
 use std::str::FromStr;
 use trust_dns::client::{Client, SyncClient};
 use trust_dns::op::DnsResponse;
 use trust_dns::rr::{DNSClass, Name, RData, Record, RecordType};
 use trust_dns::udp::UdpClientConnection;
+use failure::*;
 
-error_chain! {
+#[derive(Fail, Debug)]
+enum Errors {
+    #[fail(display = "No response.")]
+    NoResponse,
 }
 
 pub enum Provider {
@@ -20,51 +23,51 @@ impl Default for Provider {
 }
 
 impl Provider {
-    pub fn query_ipv4(&self) -> Result<Ipv4Addr> {
+    pub fn query_ipv4(&self) -> Result<Ipv4Addr, Error> {
         match self {
             Provider::OpenDNS => self.query_ipv4_opendns(),
         }
     }
 
-    pub fn query_ipv6(&self) -> Result<Ipv6Addr> {
+    pub fn query_ipv6(&self) -> Result<Ipv6Addr, Error> {
         match self {
             Provider::OpenDNS => self.query_ipv6_opendns(),
         }
     }
 
-    fn query_ipv4_opendns(&self) -> Result<Ipv4Addr> {
-        let address = "208.67.222.222:53".parse().unwrap();
-        let conn = UdpClientConnection::new(address).unwrap();
+    fn query_ipv4_opendns(&self) -> Result<Ipv4Addr, Error> {
+        let address = "208.67.222.222:53".parse()?;
+        let conn = UdpClientConnection::new(address)?;
         let client = SyncClient::new(conn);
 
-        let name = Name::from_str("myip.opendns.com.").unwrap();
+        let name = Name::from_str("myip.opendns.com.")?;
 
-        let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::A).unwrap();
+        let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::A)?;
 
         let answers: &[Record] = response.answers();
 
         if let &RData::A(ref ip) = answers[0].rdata() {
             Ok(*ip)
         } else {
-            Err("".into())
+            bail!(Errors::NoResponse)
         }
     }
 
-    fn query_ipv6_opendns(&self) -> Result<Ipv6Addr> {
-        let address = "[2620:119:35::35]:53".parse().unwrap();
-        let conn = UdpClientConnection::new(address).unwrap();
+    fn query_ipv6_opendns(&self) -> Result<Ipv6Addr, Error> {
+        let address = "[2620:119:35::35]:53".parse()?;
+        let conn = UdpClientConnection::new(address)?;
         let client = SyncClient::new(conn);
 
-        let name = Name::from_str("myip.opendns.com.").unwrap();
+        let name = Name::from_str("myip.opendns.com.")?;
 
-        let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::AAAA).unwrap();
+        let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::AAAA)?;
 
         let answers: &[Record] = response.answers();
 
         if let &RData::AAAA(ref ip) = answers[0].rdata() {
             Ok(*ip)
         } else {
-            Err("".into())
+            bail!(Errors::NoResponse)
         }
     }
 }
